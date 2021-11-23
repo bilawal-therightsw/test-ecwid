08c4616b731c24b152f70d1a28fb19dad9b4fb5c
bilawal-therightsw <bilawal.hussain@therightsw.com>
2021-11-23 18:26:10 +0500

    appending in form

--- js/assets.js
+++ js/assets.js
@@ -1,3 +1,8 @@
 Ecwid.OnPageLoaded.add(function(page){
-  console.log("Current page is of type: " + page.type);
+ 
+  if (page.type == 'CART') {
+      var html = '<div class="ec-form__row"><div class="ec-form__cell ec-form__cell--phone"><label for="ec-phone"><div class="ec-form__title ec-header-h6"><div class="marker-required marker-required--medium"></div>Phone (optional)</div></label><div class="form-control form-control--flexible form-control--empty form-control--type-phone"><input id="ec-phone" class="form-control__text" aria-label="" autocomplete="tel" required="" type="text" name="phone"><div class="form-control__placeholder"><div class="form-control__placeholder-inner"></div></div></div></div></div>';
+      var formClass = $('.ec-form').append(html);
+    console.log("Current page is of type: " + page.type);
+    }
 });
