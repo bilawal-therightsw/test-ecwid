f82b3e1f1feb81aefbdb7677a9a25747d33490ea
bilawal-therightsw <bilawal.hussain@therightsw.com>
2021-12-24 10:40:39 +0500

    CHECKOUT_ADDRESS

--- js/assets.js
+++ js/assets.js
@@ -3,10 +3,15 @@ window.localStorage.setItem("show_ec_logs","true")
 Ecwid.OnPageLoaded.add(function(page){
  
   if (page.type == 'CART') {
-    alert('hi');
-      // var html = '<div class="ec-form__row"><div class="ec-form__cell ec-form__cell--phone"><label for="ec-phone"><div class="ec-form__title ec-header-h6"><div class="marker-required marker-required--medium"></div>Phone (optional)</div></label><div class="form-control form-control--flexible form-control--empty form-control--type-phone"><input id="ec-phone" class="form-control__text" aria-label="" autocomplete="tel" required="" type="text" name="phone"><div class="form-control__placeholder"><div class="form-control__placeholder-inner"></div></div></div></div></div>';
-      // var formClass = $('.ec-form').append(html);
-      // console.log("Current page is of type: " + page.type); 
+      var html = '<div class="ec-form__row"><div class="ec-form__cell ec-form__cell--phone"><label for="ec-phone"><div class="ec-form__title ec-header-h6"><div class="marker-required marker-required--medium"></div>Phone (optional)</div></label><div class="form-control form-control--flexible form-control--empty form-control--type-phone"><input id="ec-phone" class="form-control__text" aria-label="" autocomplete="tel" required="" type="text" name="phone"><div class="form-control__placeholder"><div class="form-control__placeholder-inner"></div></div></div></div></div>';
+      var formClass = $('.ec-form').append(html);
+      console.log("Current page is of type: " + page.type); 
     }
-    console.log(page.type);
+
+    if (page.type == 'CHECKOUT_ADDRESS') {
+      var html = '<div class="ec-form__row"><div class="ec-form__cell ec-form__cell--phone"><label for="ec-phone"><div class="ec-form__title ec-header-h6"><div class="marker-required marker-required--medium"></div>Phone (optional)</div></label><div class="form-control form-control--flexible form-control--empty form-control--type-phone"><input id="ec-phone" class="form-control__text" aria-label="" autocomplete="tel" required="" type="text" name="phone"><div class="form-control__placeholder"><div class="form-control__placeholder-inner"></div></div></div></div></div>';
+      var formClass = $('.ec-form').append(html);
+      alert('here');
+      console.log("Current page is of type: " + page.type); 
+    }
 });
