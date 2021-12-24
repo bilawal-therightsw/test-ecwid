d6fc3a25f368ca3bb3acab27f8071299b54c4d73
bilawal-therightsw <bilawal.hussain@therightsw.com>
2021-12-24 11:13:42 +0500

    inserting data

--- js/assets.js
+++ js/assets.js
@@ -5,8 +5,8 @@ Ecwid.OnPageLoaded.add(function(page){
 
     if (page.type == 'CHECKOUT_ADDRESS') {
       var html = '<div class="ec-form__row"><div class="ec-form__cell ec-form__cell--phone"><label for="ec-phone"><div class="ec-form__title ec-header-h6"><div class="marker-required marker-required--medium"></div>Mobile (required)</div></label><div class="form-control form-control--flexible form-control--empty form-control--type-phone"><input id="ec-phone" class="form-control__text" aria-label="" autocomplete="tel" required="" type="text" name="phone"><div class="form-control__placeholder"><div class="form-control__placeholder-inner"></div></div></div></div></div>';
-      var formClass = $('.ec-form').append(html);
       $('.ec-form__cell--name').parent().addClass("before-mobile");
+      var formClass = $(html).insertAfter('.before-mobile');
       console.log($('.ec-form__cell--name').parent());
       console.log("Current page is of type: " + page.type); 
     }
